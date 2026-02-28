b9e66e98855c1e534495a62612365d172fb47617
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-02-28 19:04:15 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,7 +19,7 @@ bool checkPrime(uint64_t value)
   } 
   else 
   {
-    for (int i = 3; i * i <= value; ++i) 
+  for (int i = 3; i * i <= value; ++i) 
     {
       if (value % i == 0)
         return false;
@@ -56,75 +56,75 @@ uint64_t nPrime(uint64_t n)
 }
 
 uint64_t nextPrime(uint64_t value) 
-{
-  if (checkPrime(value)) 
   {
-    if (value == 2) 
+    if (checkPrime(value)) 
     {
-      return 3;
-      } 
-    else 
-    {
-      value += 2;
-      while (!checkPrime(value)) 
+      if (value == 2) 
+      {
+        return 3;
+        } 
+      else 
       {
         value += 2;
+        while (!checkPrime(value)) 
+        {
+          value += 2;
+        }
+      }
+    } 
+    else 
+    {
+      while (!checkPrime(value)) 
+      {
+        value++;
       }
     }
-  } 
-  else 
-  {
-    while (!checkPrime(value)) 
-    {
-      value++;
-    }
+    return value;
   }
-  return value;
-}
 
 uint64_t sumPrime(const uint64_t hbound) 
-{
-  if (hbound <= 2) 
   {
-    return 0;
-  } 
-  else 
-  {
-    uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i = nextPrime(i)) 
+    if (hbound <= 2) 
     {
-      sum += i;
+      return 0;
+    } 
+    else 
+    {
+      uint64_t sum = 0;
+      for (uint64_t i = 2; i < hbound; i = nextPrime(i)) 
+      {
+        sum += i;
+      }
+      return sum;
     }
-    return sum;
-  }
 }
 
 uint64_t twinPrimes(uint64_t lbound, const uint64_t hbound) 
-{
-  if (hbound < 3) 
   {
-    return 0;
-  } 
-  else if (hbound - lbound < 2) 
-  {
-    return 0;
-  } 
-  else 
-  {
-    uint64_t twin = 0;
-    while (lbound < hbound) 
+    if (hbound < 3) 
     {
-      if (!checkPrime(lbound)) 
+      return 0;
+    } 
+    else if (hbound - lbound < 2) 
+    {
+      return 0;
+    } 
+    else 
+    {
+      uint64_t twin = 0;
+      while (lbound < hbound) 
       {
+        if (!checkPrime(lbound)) 
+        {
+          lbound = nextPrime(lbound);
+        }
+        uint64_t pr = lbound;
         lbound = nextPrime(lbound);
-      }
-      uint64_t pr = lbound;
-      lbound = nextPrime(lbound);
-      if (lbound >= hbound)
-        break;
-      if (lbound - pr == 2)
-        twin++;
-      }
-    return twin;
-  }
+        if (lbound >= hbound)
+          break;
+        if (lbound - pr == 2)
+          twin++;
+        }
+      return twin;
+    }
 }
